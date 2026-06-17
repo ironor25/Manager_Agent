540e02e83abbd46397509d1c31e16f0735bedef8
Ironor25 <rock.on0103@gmail.com>
2026-06-17 09:54:26 +0530

    fixed

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -119,7 +119,12 @@
                         <div class="row g-3 mt-1">
                             <div class="col-md-12">
                                 <label class="form-label fw-semibold text-body">Team</label>
-                                <input type="text" name="team" class="form-control" required placeholder="e.g. Engineering">
+                                 <select name="team" class="form-select" required>
+                                    <option value="" disabled selected>Select a Team</option>
+                                    @foreach($teams as $team)
+                                        <option value="{{ $team->name }}">{{ $team->name }}</option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
                 </div>
